911b163f49fafdbfa0b92e1c936ab154b76e1e71
Aryan-Arora <72548611+Aryan-Arora@users.noreply.github.com>
2021-11-18 10:23:17 +0530

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,16 @@
+
+
+function calculateTotal(quantity,price){
+return quantity*price
+   
+}
+var subtotal=0;
+function outputCartRow(subtotal,tax,shipping,grand_total){
+    
+    
+}
+
+function outputSubtotoal(subtotal){
+subtotal=quantities[3,1,2]*prices[80,125,75]
+document.write(subtotal)
+}
